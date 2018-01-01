23197096b21d56c38a494a2fc0b55e35f1b8d093
jawache <jawache@gmail..com>
2018-01-01 21:59:50 +0000

    Removed References to AngularJS

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,19 +1,3 @@
-import 'angular';
-import 'angular-resource';
-import 'angular-animate';
-import 'ng-infinite-scroll';
-import 'angular-spinner';
-import 'angular-auto-validate/dist/jcs-auto-validate';
-import 'angular-ladda';
-import 'angular-strap';
-import 'angularjs-toaster';
-import 'angular-ui-router';
-
-import './app.main';
-// import './services';
-import './filters';
-// import './components';
-
 import './polyfills.ts';
 
 import { NgModule } from '@angular/core';
