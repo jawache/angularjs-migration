53b6069d392c1d92fcb5511abea6a994ca61485e
jawache <jawache@gmail..com>
2018-01-02 13:07:15 +0000

    Final clean up and bundle css

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,14 @@
 const path = require('path');
 const webpack = require('webpack');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const extractSass = new ExtractTextPlugin({
+    filename: "src/dist/bundle.css",
+    disable: process.env.NODE_ENV === "development"
+});
 
 module.exports = {
-    entry: "./src/app/main.ts",
+    entry: ["./src/app/main.ts", "./src/css/main.scss"],
     output: {
         filename: "src/dist/bundle.js"
     },
@@ -13,10 +19,26 @@ module.exports = {
     module: {
         loaders: [
             // all files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'
-            { test: /\.tsx?$/, loader: "ts-loader" }
+            { 
+                test: /\.tsx?$/, 
+                loader: "ts-loader" 
+            },
+            {
+                test: /\.scss$/,
+                use: extractSass.extract({
+                    use: [{
+                        loader: "css-loader"
+                    }, {
+                        loader: "sass-loader"
+                    }],
+                    // use style-loader in development
+                    fallback: "style-loader"
+                })
+            }
         ]
     },
     plugins: [
+        extractSass,
         new webpack.ContextReplacementPlugin(
             // if you have anymore problems tweet me at @gdi2290
             // The (\\|\/) piece accounts for path separators for Windows and MacOS
