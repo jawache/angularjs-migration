eda0303f3a39abbcffa0dd5346746a03a998e770
Finley Williamson V <finley.williamson@gmail.com>
2020-09-02 12:28:02 -0400

    Improvements to Contact Service

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -18,89 +18,148 @@ export interface IContact {
     favorite: boolean
 }
 
+/**
+ * Class ContactService
+ */
+export class ContactService {
+
+    // private member vars
+    private page: number
+    private hasMore: boolean
+    private isLoading: boolean
+    // private selectedPerson: IContact     // why do we need this
+    private persons: IContact[]
+    private search: unknown
+    private sorting: string
+    private ordering: string
+
+    // public memeber vars
+    public isSaving: boolean
+    public isDeleting: boolean
+
+    /**
+     * Constructor
+     * @param ContactDB -- injected 
+     * @param toaster  -- injected
+     */
+    constructor( private ContactDB: ContactDB, private toaster ) {
+        this.page = 1;
+        this.hasMore = true;
+        this.isLoading = false;
+        this.isSaving = false;
+        this.isDeleting = false;
+        // this.selectedPerson = null;     // why do we need this
+        this.persons = [];
+        this.search = null;
+        this.sorting = 'name';
+        this.ordering = 'ASC';
+
+        this.loadContacts()
+    }
+
+    /**
+     * Returns a Contact from the DB
+     * @param email 
+     */
+    public getPerson = ( email: string ): IContact => {
+        return _.find( this.persons, ( person: IContact ) => person.email == email )
+    }
+
+    /**
+     * Performs the search on the db
+     */
+    public doSearch = (): void => {
+        this.hasMore = true;
+        this.page = 1;
+        this.persons = [];
+        this.loadContacts();
+    }
+
+    /**
+     * Performs the ordering on the results
+     */
+    public doOrder = (): void => {
+        this.hasMore = true;
+        this.page = 1;
+        this.persons = [];
+        this.loadContacts();
+    }
+
+    /**
+     * Loads current set of contacts from the db
+     */
+    public loadContacts = async (): Promise<void> => {
+        if ( this.hasMore && !this.isLoading ) {
+            this.isLoading = true;
+
+            const params: ContactRouteParams = {
+                _page: this.page,
+                _sort: this.sorting,
+                _order: this.ordering,
+                q: this.search
+            };
+
+            const response: angular.IHttpResponse<IContact[]> = await this.ContactDB.query( params )
+            this.persons.push( ...response.data )
+
+            if ( response.data.length === 0 ) {
+                this.hasMore = false;
+            }
+            this.isLoading = false;
+        }
+    }
+
+    /**
+     * Loads the next page of contacts
+     */
+    public loadMore = (): void => {
+        if ( this.hasMore && !this.isLoading ) {
+            this.page += 1;
+            this.loadContacts();
+        }
+    }
+
+    /**
+     * Updates the contact info for a particular person
+     * @param person 
+     */
+    public updateContact = async ( person ): Promise<void> => {
+        this.isSaving = true;
+        await this.ContactDB.update( person )
+        this.isSaving = false;
+        this.toaster.pop( "success", "Updated " + person.name );
+    }
+
+    /**
+     * Removes a contact from the DB
+     * @param person 
+     */
+    public removeContact = async ( person ): Promise<void> => {
+        this.isDeleting = true;
+        const name: string = person.name;
+        await this.ContactDB.remove( person )
+        this.isDeleting = false;
+        const index: number = this.persons.indexOf( person );
+        this.persons.splice( index, 1 );
+        this.toaster.pop( "success", "Deleted " + name );
+    }
+
+    /**
+     * Creates a contact and adds it to the DB
+     * @param person 
+     */
+    public createContact = async ( person ): Promise<void> => {
+        this.isSaving = true;
+        await this.ContactDB.save( person )
+        this.isSaving = false;
+        this.hasMore = true;
+        this.page = 1;
+        this.persons = [];
+        this.loadContacts();
+        this.toaster.pop( "success", "Created " + person.name );
+    }
+}
+
 angular
     .module( "codecraft" )
-    .factory( "ContactService", function ( ContactDB: ContactDB, $rootScope, $q, toaster ) {
-        const self = {
-            getPerson: ( email: string ): IContact => {
-                return _.find(self.persons, (person: IContact) => person.email == email)
-            },
-            page: 1,
-            hasMore: true,
-            isLoading: false,
-            isSaving: false,
-            isDeleting: false,
-            persons: [],
-            search: null,
-            sorting: "name",
-            ordering: "ASC",
-            doSearch: function () {
-                self.hasMore = true;
-                self.page = 1;
-                self.persons = [];
-                self.loadContacts();
-            },
-            doOrder: function () {
-                self.hasMore = true;
-                self.page = 1;
-                self.persons = [];
-                self.loadContacts();
-            },
-            loadContacts: async function () {
-                if ( self.hasMore && !self.isLoading ) {
-                    self.isLoading = true;
-
-                    const params: ContactRouteParams = {
-                        _page: self.page,
-                        _sort: self.sorting,
-                        _order: self.ordering,
-                        q: self.search
-                    };
-
-                    const response: angular.IHttpResponse<IContact[]> = await ContactDB.query( params )
-                    self.persons.push(...response.data)
-
-                    if ( response.data.length === 0 ) {
-                        self.hasMore = false;
-                    }
-                    self.isLoading = false;
-                    
-                }
-            },
-            loadMore: (): void => {
-                if ( self.hasMore && !self.isLoading ) {
-                    self.page += 1;
-                    self.loadContacts();
-                }
-            },
-            updateContact: async ( person ): Promise<void> => {
-                self.isSaving = true;
-                await ContactDB.update(person)
-                self.isSaving = false;
-                toaster.pop( "success", "Updated " + person.name );
-            },
-            removeContact: async ( person ): Promise<void> => {
-                self.isDeleting = true;
-                const name: string = person.name;
-                await ContactDB.remove(person)
-                self.isDeleting = false;
-                const index: number = self.persons.indexOf( person );
-                self.persons.splice( index, 1 );
-                toaster.pop( "success", "Deleted " + name );
-            },
-            createContact: async ( person ): Promise<void> => {
-                self.isSaving = true;
-                await ContactDB.save( person )
-                self.isSaving = false;
-                self.hasMore = true;
-                self.page = 1;
-                self.persons = [];
-                self.loadContacts();
-                toaster.pop( "success", "Created " + person.name );
-            }
-        };
-
-        self.loadContacts();
-
-        return self;
-    } );
+    .service( "ContactService", ContactService )
